8df5997ec43999433890fc84767a9ddaa5b61661
sunny-aguilar <sunny.aguilar@gmail.com>
2022-03-18 23:02:54 -0700

    go-cards

--- deck.go
+++ deck.go
@@ -8,6 +8,22 @@ import (
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spaces", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six"}
+
+	// _ means variable acknowledge but not needed
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
