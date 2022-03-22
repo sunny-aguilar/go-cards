472d49c79152e4e30094c15be3817b688e57c3b3
sunny-aguilar <sunny.aguilar@gmail.com>
2022-03-22 15:30:46 -0700

    go-cards

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-func main() {
-	cards := newDeck()
+// import "fmt"
 
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
+func main() {
+	// cards := newDeck()
+
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+
+	cards := newDeck()
+	cards.saveToFile("my_cards")
+	// fmt.Println(cards.toString())
+
+	// type conversion
+	// greeting := "Hi there!"
+	// fmt.Println([]byte(greeting))
 }
